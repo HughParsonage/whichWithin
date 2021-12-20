cef00fb29a18e87cded2ab0f5f377ec1eb3e854f
HughParsonage <hugh.parsonage@gmail.com>
2021-12-21 01:23:08 +1100

    na-handling

--- src/na-handling.c
+++ src/na-handling.c
@@ -1,15 +1,21 @@
 #include "whichWithin.h"
 
-SEXP Cfirst_non_na_dbl(SEXP xx, SEXP ffill) {
+double first_non_na_dbl(SEXP xx, SEXP ffill) {
   if (!isReal(xx) || !isReal(ffill)) {
     error("(Cfirst_non_na_dbl): must be REALSXP"); // # nocov
   }
   R_xlen_t N = xlength(xx);
+  const double fill = asReal(ffill);
+  const double * x = REAL(xx);
   for (R_xlen_t i = 0; i < N; ++i) {
     if (!ISNAN(x[i])) {
       return x[i];
     }
   }
   return fill;
+}
+
+SEXP Cfirst_non_na_dbl(SEXP xx, SEXP ffill) {
+  return ScalarReal(first_non_na_dbl(xx, ffill));
 }
 
